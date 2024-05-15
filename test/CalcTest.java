34df89fd42f200be0b2c4947ee8184186b7dabb6
murakami <murakami@brainsellers.com>
2024-05-15 14:11:47 +0900

    テスト1

---
+++ test/CalcTest.java
@@ -0,0 +1,10 @@
+
+public class CalcTest {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		double ret = 13D * 1D / 1000D * (25.4D/72D);
+		System.out.println(ret);
+	}
+
+}
